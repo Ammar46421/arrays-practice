6daf875c7de7160390cc40a129a2623cd3d3307d
Ammar46421 <153704851+Ammar46421@users.noreply.github.com>
2024-06-30 17:44:36 +0500

    summary

---
+++ index.js
@@ -0,0 +1,17 @@
+var cities = ["karachi" ,"lahore" ,"sukkur" ,"islamabad"]
+console.log(cities)
+
+var arr = [];
+var weekDays = ["monday", "tuesday", "wednesday", "thursday", "friday"];
+console.log(weekDays[4]);
+var countries = ["pakistan", "china", "Turkey", "japan"];
+countries.push("india")
+// countries.pop()
+countries[2] = "Turkiye";
+console.log(countries);
+var mixedValues = [1, "foo", 4, null, false, undefined];
+
+console.log(weekDays[55]);
+
+
+
